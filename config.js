4e48effeeea4be961cda1e61c8da7f136d876c45
nixmin23 <86486020+nixmin23@users.noreply.github.com>
2022-02-13 14:55:01 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyDu-LlFbg6TEQCpLbqBBfCEOad_fYXRQ9E",
+    authDomain: "e-ride-project-e7b16.firebaseapp.com",
+    projectId: "e-ride-project-e7b16",
+    storageBucket: "e-ride-project-e7b16.appspot.com",
+    messagingSenderId: "933614318798",
+    appId: "1:933614318798:web:60f701549048c6781c8c9f"
+  };
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
